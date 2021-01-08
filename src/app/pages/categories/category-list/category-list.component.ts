08748bea1ba707d952e94b2a8fd7f9db33b2524c
Maria Sheila Carneiro <msheilacarneiro@gmail.com>
2021-01-07 21:19:13 -0300

    sistema financias
    sistema financias

---
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Category } from "../shared/category.model";
+import { CategoryService } from "../shared/category.service";
+import { element } from 'protractor';
+
+@Component({
+  selector: 'app-category-list',
+  templateUrl: './category-list.component.html',
+  styleUrls: ['./category-list.component.scss']
+})
+export class CategoryListComponent implements OnInit {
+
+  categories: Category[] = [];
+
+  constructor(private categoryService: CategoryService) { }
+
+  ngOnInit() {
+    this.categoryService.getAll().subscribe(
+      categories => this.categories = categories,
+      error => alert('Erro ao carregar a lista')
+    )
+  }
+
+  //Método Deletar Categoria
+  deleteCategory(category){
+    const mustDelete = confirm('Deseja realamnte excluir este item?');
+
+    if(mustDelete){
+      this.categoryService.delete(category.id).subscribe(
+        () => this.categories = this.categories.filter(element => element != category),
+        () => alert("Erro ao tentar excluir!")
+      )
+    }
+  }
+
+}
